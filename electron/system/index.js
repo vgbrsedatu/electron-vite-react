893bf39df03e0eb02e76fd02d9c48777bcf69a90
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-07 22:31:04 -0600

    feat: add system module

---
+++ electron/system/index.js
@@ -0,0 +1,11 @@
+/**
+ * @author Victor Giovanni Beltrán Rodríguez
+ * @file Main file to export all electron system to main proccess.
+ */
+
+// ━━ EXPORT PACKAGES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+exports.dialog = require('./dialog');
+exports.file = require('./file');
+exports.menu = require('./menu');
+exports.notification = require('./notification');
+exports.tray = require('./tray');
