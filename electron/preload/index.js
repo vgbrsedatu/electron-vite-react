41ec1249de8e7bde6afd33cfa9df5551cac2aee2
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-08 01:28:25 -0600

    style: edit comment

--- electron/preload/index.js
+++ electron/preload/index.js
@@ -5,7 +5,7 @@
  * as Electron's renderer process modules and some polyfilled Node.js functions.
  *
  */
-// ━━ IMPORT PACKAGES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // » IMPORT ELECTRON APIS
 const { contextBridge, ipcRenderer } = require('electron');
 
