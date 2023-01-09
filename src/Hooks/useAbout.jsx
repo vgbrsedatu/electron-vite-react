9d171001ef1d3e074e8aa39443ba97bba39a9148
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-08 18:36:36 -0600

    feat: add useAbout a custom React hook

---
+++ src/Hooks/useAbout.jsx
@@ -0,0 +1,41 @@
+/**
+ * @author Victor Giovanni Beltrán Rodríguez
+ * @file Contains `useWindowState` a custom Reack Hook.
+ */
+// ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// » IMPORT REACT MODULES
+import { useEffect, useState } from 'react';
+
+// ━━ TYPE DEFINITIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The returns value from `useAbout`
+ *
+ * @typedef   {Object}  AboutResponse
+ * @property  {string}  name          - Application name.
+ * @property  {string}  description   - Application description.
+ * @property  {string}  team          - The team that developed the application.
+ * @property  {string}  company       - Name of the company that developed the application.
+ * @property  {string}  copyright     - Application copyright.
+ * @property  {string}  version       - Application version.
+ * @property  {string}  author        - Main author that developed the application.
+ */
+
+// ━━ COMPONENT ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The `useAbout` it's a custom React hook witch communicates with the
+ * `electron` api, used to manages about state.
+ *
+ * @returns {AboutResponse} Information about the application.
+ */
+const useAbout = () => {
+  const [about, setAbout] = useState(null);
+
+  useEffect(() => {
+    setAbout(window.appRuntime.about);
+  }, []);
+
+  return { about };
+};
+
+// ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+export default useAbout;
