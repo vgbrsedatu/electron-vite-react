1d12ee601905c1a0a31c8344c658d4e6c670b33e
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-09 21:18:06 -0600

    fix: rename custom hook

--- src/Components/Layout/Header/TitleBar/index.jsx
+++ src/Components/Layout/Header/TitleBar/index.jsx
@@ -10,7 +10,7 @@ import MinimizeButton from './MinimizeButton';
 import CloseButton from './CloseButton';
 
 // » IMPORT CUSTOM HOOKS
-import useWindowState from '../../../../Hooks/useWindowState';
+import useWindow from '../../../../Hooks/useWindow';
 
 // ━━ COMPONENT ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
@@ -20,7 +20,7 @@ import useWindowState from '../../../../Hooks/useWindowState';
  * @returns {JSX.Element} The `TitleBar` components.
  */
 const TitleBar = () => {
-  const { minimizeWindow, closeWindow } = useWindowState();
+  const { minimizeWindow, closeWindow } = useWindow();
   return (
     <div className="title-bar">
       <AppName />
