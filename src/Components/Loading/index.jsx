5a51391b688b69db8a808f347a1240e4bb58c208
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-11 00:13:32 -0600

    feat: add Loading component

---
+++ src/Components/Loading/index.jsx
@@ -0,0 +1,16 @@
+/**
+ * @author Victor Giovanni Beltrán Rodríguez
+ * @file Manage `Loading` React component.
+ */
+
+// ━━ COMPONENT ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The `Loading` component.
+ *
+ * @component
+ * @returns {JSX.Element} The `Loading` components.
+ */
+const Loading = () => <section className="loading" />;
+
+// ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+export default Loading;
